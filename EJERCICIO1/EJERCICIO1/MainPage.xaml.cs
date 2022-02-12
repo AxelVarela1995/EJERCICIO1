3335171d2fde62b770e2a30bbf5cf397b96ae8bc
Axel Varela <axelvarela66@gmail.com>
2022-02-11 18:47:49 -0600

    Add project files.

---
+++ EJERCICIO1/EJERCICIO1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCICIO1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnpagina_Clicked(object sender, EventArgs e)
+        {
+            var Contact = new Models.Contactos
+            {
+                Nombre = this.txtnombre.Text,
+                Apellidos = this.txtapellido.Text,
+                Edad = Convert.ToInt32(this.txtedad.Text),
+                Correo = txtcorreo.Text
+            };
+            var pagina = new PageMain();
+            pagina.BindingContext = Contact;
+            await Navigation.PushAsync(pagina);
+        }
+    }
+}
---
+++ EJERCICIO1/EJERCICIO1/Models/Contactos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO1.Models
+{
+    public class Contactos
+    {
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+        public Int32 Edad { get; set; }
+        public String Correo { get; set; }
+    
+    }
+}
